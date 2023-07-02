dcd6b603314a4963169e07f6985b13adf96b58a0
Luiz Felipe <luizfelipemourab@gmail.com>
2023-07-02 10:54:01 -0300

    Add files via upload

---
+++ ExemploWhile.php
@@ -0,0 +1,15 @@
+<?php
+$x = 0;
+
+while ($x < 10) {
+    echo "x = $x \n";
+    
+    if ($x == 7) {
+        break;
+        
+    }//fim do if
+    
+$x ++; //operador de incremento
+    
+}//fim do while
+?>
